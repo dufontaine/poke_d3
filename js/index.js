58e43628246bfd0ed30ceb8e68938bcdaad638e0
Dustin Fontaine <dustin.fontaine12@gmail.com>
2017-05-02 00:56:13 -0500

    heatmap v1

--- js/index.js
+++ js/index.js
@@ -12,6 +12,7 @@ d3.csv("pokeSTATS.csv", function(loadedData) {
     LoadPics(filteredData);
     createListeners(); //had to add listeners programatically to programatically created objects
     makeRadar(filteredData);
+    UpdateTypeChart();
 });
 
 function createListeners() {
@@ -45,7 +46,6 @@ function createListeners() {
                 generateBarGraph('#dashboard-stats');
             }
         }
-        console.log(myDescription);
         
         var svg = d3.select('#one_Poke_pic') //make new svg 'canvas'
         // define background patterns
@@ -90,6 +90,7 @@ function createListeners() {
         LoadPics(filteredData);
         createListeners();
         makeRadar(filteredData);
+        UpdateTypeChart();
     });
     
     //double click on pokemon to remove from selected
@@ -105,6 +106,7 @@ function createListeners() {
         LoadPics(filteredData);
         createListeners();
         makeRadar(filteredData);
+        UpdateTypeChart();
     });  
 };
 
@@ -235,8 +237,8 @@ function makeRadar (myData) {
     //set up configuration for radar chart
     RadarChart.defaultConfig.color = function() {};
     RadarChart.defaultConfig.radius = 3;
-    RadarChart.defaultConfig.w = 300;
-    RadarChart.defaultConfig.h = 300;
+    RadarChart.defaultConfig.w = 400;
+    RadarChart.defaultConfig.h = 400;
     RadarChart.defaultConfig.maxValue = 100;
     RadarChart.defaultConfig.levels = 4;
     RadarChart.defaultConfig.circles=true;
@@ -252,6 +254,178 @@ function makeRadar (myData) {
     //render();
 }
 
+function UpdateTypeChart() {
+    var attack_strength = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    var attack_weak = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    var defense_resist = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    var defense_weak = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+
+    //loop through each selected pokemon and gather type strengths and weaknesses
+    for (i=0; i<filteredData.length; i++) {
+        myPoke = filteredData[i];
+        if (myPoke.Number != "999") {
+            if(myPoke["Type 1"] == "Normal" | myPoke["Type 2"] == "Normal") {
+                attack_strength[12] += 1; attack_strength[13] += 1;
+                defense_weak[6] += 1;
+                defense_resist[13] += 1;
+            }
+            if(myPoke["Type 1"] == "Fire" | myPoke["Type 2"] == "Fire") {
+                attack_strength[4] += 1; attack_strength[5] += 1; attack_strength[11] += 1;
+                attack_weak[1] += 1; attack_weak[2] += 1; attack_weak[12] += 1; attack_weak[14] += 1;
+                defense_weak[2] += 1; defense_weak[8] += 1; defense_weak[12] += 1;
+                defense_resist[1] += 1; defense_resist[4] += 1; defense_resist[11] += 1;
+            }
+            if(myPoke["Type 1"] == "Water" | myPoke["Type 2"] == "Water") {
+                attack_strength[1] += 1; attack_strength[8] += 1; attack_strength[12] += 1;
+                attack_weak[2] += 1; attack_weak[4] += 1; attack_weak[14] += 1;
+                defense_weak[3] += 1; defense_weak[4] += 1;
+                defense_resist[1] += 1; defense_resist[2] += 1; defense_resist[5] += 1;
+            }
+            if(myPoke["Type 1"] == "Electric" | myPoke["Type 2"] == "Electric") {
+                attack_strength[2] += 1; attack_strength[9] += 1;
+                attack_weak[3] += 1; attack_weak[4] += 1; attack_weak[8] += 1; attack_weak[14] += 1;
+                defense_weak[8] += 1;
+                defense_resist[3] += 1; defense_resist[9] += 1;
+            }
+            if(myPoke["Type 1"] == "Grass" | myPoke["Type 2"] == "Grass") {
+                attack_strength[2] += 1; attack_strength[8] += 1; attack_strength[12] += 1;
+                attack_weak[1] += 1; attack_weak[4] += 1; attack_weak[7] += 1; attack_weak[9] += 1; attack_weak[11] += 1; attack_weak[14] += 1;
+                defense_weak[1] += 1; defense_weak[5] += 1; defense_weak[7] += 1; defense_weak[9] += 1; defense_weak[11] += 1;
+                defense_resist[2] += 1; defense_resist[3] += 1; defense_resist[4] += 1; defense_resist[8] += 1;
+            }
+            if(myPoke["Type 1"] == "Ice" | myPoke["Type 2"] == "Ice") {
+                attack_strength[4] += 1; attack_strength[8] += 1; attack_strength[9] += 1; attack_strength[14] += 1;
+                attack_weak[2] += 1; attack_weak[5] += 1;
+                defense_weak[1] += 1; defense_weak[6] += 1; defense_weak[12] += 1;
+                defense_resist[5] += 1;
+            }
+            if(myPoke["Type 1"] == "Fighting" | myPoke["Type 2"] == "Fighting") {
+                attack_strength[0] += 1; attack_strength[5] += 1; attack_strength[12] += 1;
+                attack_weak[7] += 1; attack_weak[9] += 1; attack_weak[10] += 1; attack_weak[11] += 1; attack_weak[13] += 1;
+                defense_weak[9] += 1; defense_weak[10] += 1;
+                defense_resist[11] += 1; defense_resist[12] += 1;
+            }
+            if(myPoke["Type 1"] == "Poison" | myPoke["Type 2"] == "Poison") {
+                attack_strength[4] += 1; attack_strength[11] += 1;
+                attack_weak[7] += 1; attack_weak[8] += 1; attack_weak[12] += 1; attack_weak[13] += 1;
+                defense_weak[8] += 1; defense_weak[10] += 1; defense_weak[11] += 1;
+                defense_resist[4] += 1; defense_resist[6] += 1; defense_resist[7] += 1;
+            }
+            if(myPoke["Type 1"] == "Ground" | myPoke["Type 2"] == "Ground") {
+                attack_strength[1] += 1; attack_strength[3] += 1; attack_strength[7] += 1; attack_strength[12] += 1;
+                attack_weak[4] += 1; attack_weak[9] += 1; attack_weak[11] += 1;
+                defense_weak[2] += 1; defense_weak[4] += 1; defense_weak[5] += 1;
+                defense_resist[3] += 1; defense_resist[7] += 1; defense_resist[12] += 1;
+            }
+            if(myPoke["Type 1"] == "Flying" | myPoke["Type 2"] == "Flying") {
+                attack_strength[4] += 1; attack_strength[6] += 1; attack_strength[11] += 1;
+                attack_weak[3] += 1; attack_weak[12] += 1;
+                defense_weak[3] += 1; defense_weak[5] += 1; defense_weak[12] += 1;
+                defense_resist[4] += 1; defense_resist[6] += 1; defense_resist[8] += 1; defense_resist[11] += 1;
+            }
+            if(myPoke["Type 1"] == "Psychic" | myPoke["Type 2"] == "Psychic") {
+                attack_strength[6] += 1; attack_strength[7] += 1;
+                attack_weak[10] += 1;
+                defense_weak[11] += 1;
+                defense_resist[6] += 1; defense_resist[10] += 1; defense_resist[13] += 1;
+            }
+            if(myPoke["Type 1"] == "Bug" | myPoke["Type 2"] == "Bug") {
+                attack_strength[4] += 1; attack_strength[7] += 1; attack_strength[10] += 1;
+                attack_weak[1] += 1; attack_weak[6] += 1; attack_weak[9] += 1;
+                defense_weak[1] += 1; defense_weak[7] += 1; defense_weak[9] += 1; defense_weak[12] += 1;
+                defense_resist[4] += 1; defense_resist[6] += 1; defense_resist[8] += 1;
+            }
+            if(myPoke["Type 1"] == "Rock" | myPoke["Type 2"] == "Rock") {
+                attack_strength[1] += 1; attack_strength[5] += 1; attack_strength[9] += 1; attack_strength[11] += 1;
+                attack_weak[6] += 1; attack_weak[8] += 1;
+                defense_weak[2] += 1; defense_weak[4] += 1; defense_weak[6] += 1; defense_weak[8] += 1;
+                defense_resist[0] += 1; defense_resist[1] += 1; defense_resist[7] += 1; defense_resist[9] += 1;
+            }
+            if(myPoke["Type 1"] == "Ghost" | myPoke["Type 2"] == "Ghost") {
+                attack_strength[13] += 1;
+                attack_weak[0] += 1; attack_weak[10] += 1;
+                defense_weak[13] += 1;
+                defense_resist[0] += 1; defense_resist[6] += 1; defense_resist[7] += 1;
+            }
+            if(myPoke["Type 1"] == "Dragon" | myPoke["Type 2"] == "Dragon") {
+                attack_strength[14] += 1;
+                
+                defense_weak[5] += 1; defense_weak[14] += 1;
+                defense_resist[1] += 1; defense_resist[2] += 1; defense_resist[3] += 1; defense_resist[4] += 1;
+            }
+            
+        }
+    }
+    
+    //update colors on type chart
+    Gcolz = ['#ffffff','#ccece6','#99d8c9','#66c2a4','#41ae76','#238b45','#006d2c','#006d2c','#006d2c','#006d2c'];
+    Rcolz = ['#ffffff','#fcbba1','#fc9272','#fb6a4a','#ef3b2c','#cb181d','#a50f15','#a50f15','#a50f15','#a50f15'];
+    
+    document.getElementById("A_normal").style.backgroundColor = Gcolz[attack_strength[0]];
+    document.getElementById("A_fire").style.backgroundColor = Gcolz[attack_strength[1]];
+    document.getElementById("A_water").style.backgroundColor = Gcolz[attack_strength[2]];
+    document.getElementById("A_electric").style.backgroundColor = Gcolz[attack_strength[3]];
+    document.getElementById("A_grass").style.backgroundColor = Gcolz[attack_strength[4]];
+    document.getElementById("A_ice").style.backgroundColor = Gcolz[attack_strength[5]];
+    document.getElementById("A_fight").style.backgroundColor = Gcolz[attack_strength[6]];
+    document.getElementById("A_poison").style.backgroundColor = Gcolz[attack_strength[7]];
+    document.getElementById("A_ground").style.backgroundColor = Gcolz[attack_strength[8]];
+    document.getElementById("A_flying").style.backgroundColor = Gcolz[attack_strength[9]];
+    document.getElementById("A_psychic").style.backgroundColor = Gcolz[attack_strength[10]];
+    document.getElementById("A_bug").style.backgroundColor = Gcolz[attack_strength[11]];
+    document.getElementById("A_rock").style.backgroundColor = Gcolz[attack_strength[12]];
+    document.getElementById("A_ghost").style.backgroundColor = Gcolz[attack_strength[13]];
+    document.getElementById("A_dragon").style.backgroundColor = Gcolz[attack_strength[14]];
+
+    document.getElementById("B_normal").style.backgroundColor = Rcolz[attack_weak[0]];
+    document.getElementById("B_fire").style.backgroundColor = Rcolz[attack_weak[1]];
+    document.getElementById("B_water").style.backgroundColor = Rcolz[attack_weak[2]];
+    document.getElementById("B_electric").style.backgroundColor = Rcolz[attack_weak[3]];
+    document.getElementById("B_grass").style.backgroundColor = Rcolz[attack_weak[4]];
+    document.getElementById("B_ice").style.backgroundColor = Rcolz[attack_weak[5]];
+    document.getElementById("B_fight").style.backgroundColor = Rcolz[attack_weak[6]];
+    document.getElementById("B_poison").style.backgroundColor = Rcolz[attack_weak[7]];
+    document.getElementById("B_ground").style.backgroundColor = Rcolz[attack_weak[8]];
+    document.getElementById("B_flying").style.backgroundColor = Rcolz[attack_weak[9]];
+    document.getElementById("B_psychic").style.backgroundColor = Rcolz[attack_weak[10]];
+    document.getElementById("B_bug").style.backgroundColor = Rcolz[attack_weak[11]];
+    document.getElementById("B_rock").style.backgroundColor = Rcolz[attack_weak[12]];
+    document.getElementById("B_ghost").style.backgroundColor = Rcolz[attack_weak[13]];
+    document.getElementById("B_dragon").style.backgroundColor = Rcolz[attack_weak[14]];
+
+    document.getElementById("C_normal").style.backgroundColor = Rcolz[defense_weak[0]];
+    document.getElementById("C_fire").style.backgroundColor = Rcolz[defense_weak[1]];
+    document.getElementById("C_water").style.backgroundColor = Rcolz[defense_weak[2]];
+    document.getElementById("C_electric").style.backgroundColor = Rcolz[defense_weak[3]];
+    document.getElementById("C_grass").style.backgroundColor = Rcolz[defense_weak[4]];
+    document.getElementById("C_ice").style.backgroundColor = Rcolz[defense_weak[5]];
+    document.getElementById("C_fight").style.backgroundColor = Rcolz[defense_weak[6]];
+    document.getElementById("C_poison").style.backgroundColor = Rcolz[defense_weak[7]];
+    document.getElementById("C_ground").style.backgroundColor = Rcolz[defense_weak[8]];
+    document.getElementById("C_flying").style.backgroundColor = Rcolz[defense_weak[9]];
+    document.getElementById("C_psychic").style.backgroundColor = Rcolz[defense_weak[10]];
+    document.getElementById("C_bug").style.backgroundColor = Rcolz[defense_weak[11]];
+    document.getElementById("C_rock").style.backgroundColor = Rcolz[defense_weak[12]];
+    document.getElementById("C_ghost").style.backgroundColor = Rcolz[defense_weak[13]];
+    document.getElementById("C_dragon").style.backgroundColor = Rcolz[defense_weak[14]];
+    
+    document.getElementById("D_normal").style.backgroundColor = Gcolz[defense_resist[0]];
+    document.getElementById("D_fire").style.backgroundColor = Gcolz[defense_resist[1]];
+    document.getElementById("D_water").style.backgroundColor = Gcolz[defense_resist[2]];
+    document.getElementById("D_electric").style.backgroundColor = Gcolz[defense_resist[3]];
+    document.getElementById("D_grass").style.backgroundColor = Gcolz[defense_resist[4]];
+    document.getElementById("D_ice").style.backgroundColor = Gcolz[defense_resist[5]];
+    document.getElementById("D_fight").style.backgroundColor = Gcolz[defense_resist[6]];
+    document.getElementById("D_poison").style.backgroundColor = Gcolz[defense_resist[7]];
+    document.getElementById("D_ground").style.backgroundColor = Gcolz[defense_resist[8]];
+    document.getElementById("D_flying").style.backgroundColor = Gcolz[defense_resist[9]];
+    document.getElementById("D_psychic").style.backgroundColor = Gcolz[defense_resist[10]];
+    document.getElementById("D_bug").style.backgroundColor = Gcolz[defense_resist[11]];
+    document.getElementById("D_rock").style.backgroundColor = Gcolz[defense_resist[12]];
+    document.getElementById("D_ghost").style.backgroundColor = Gcolz[defense_resist[13]];
+    document.getElementById("D_dragon").style.backgroundColor = Gcolz[defense_resist[14]];
+}
+
 //Bar Chart Code ____________________________________________________________
 //(function($) {
   function generateBarGraph(wrapper) {
@@ -263,7 +437,6 @@ function makeRadar (myData) {
       values.push($(this).data('value'));
     });
 
-      console.log(values);
     // Get Max Value From Array
     //var max_value = Math.max.apply(Math, values);
 
